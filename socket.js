1b0abd6b5823c08c870939da6ee8e4e12b351e42
kwb020312 <kwb020312@naver.com>
2019-08-31 18:36:54 +0900

    HomeWork-Final(4)

--- socket.js
+++ socket.js
@@ -75,5 +75,8 @@ module.exports = (server, app, sessionMiddleware) => {
     socket.on('dm', (data) => {
       socket.to(data.target).emit('dm', data);
     });
+    socket.on('ban', (data) => {
+      socket.to(data.id).emit('ban');
+    });
   });
 };
